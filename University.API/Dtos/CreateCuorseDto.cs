d80d3c40f77c80e3e679b7f8e66927e0273cf332
hikmatillo77 <abdullayevh432@gmail.com>
2023-12-11 16:27:47 +0500

    Create Student, Course Dtos

---
+++ University.API/Dtos/CreateCuorseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace University.API.Dtos
+{
+    public class CreateCuorseDto
+    {
+        public string Name { get; set; }
+        public double Level { get; set; }
+        public string Duration { get; set; }
+    }
+}
---
+++ University.API/Dtos/GetStudentDto.cs
@@ -0,0 +1,26 @@
+﻿using University.API.Entities;
+
+namespace University.API.Dtos
+{
+    public class GetStudentDto
+    {
+        public GetStudentDto(Student entity)
+        {
+            Id = entity.Id;
+            Fullname = entity.Fullname;
+            PhoneNumber = entity.PhoneNumber;
+            Age = entity.Age;
+            Direction = entity.Direction;
+            Degree = entity.Degree;
+            FacultyId = entity.FacultyId;
+
+        }
+        public Guid Id { get; set; }
+        public string Fullname { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public string Direction { get; set; }
+        public int Degree { get; set; }
+        public int FacultyId { get; set; }
+    }
+}
