1bd819fa399dd75be5068835165d5f8d385171e4
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-12-13 15:15:47 +0500

    Write StudentService

--- University.API/Services/StudentServise.cs
+++ University.API/Services/StudentServise.cs
@@ -1,33 +1,95 @@
-﻿using University.API.Dtos;
+﻿using Microsoft.EntityFrameworkCore;
+using University.API.Data;
+using University.API.Dtos;
 using University.API.Entities;
 
 namespace University.API.Services
 {
     public class StudentServise : IStudentService
     {
-        public Task<Student> CreateStudent(CreateStudentDto newStudent)
+        private readonly AppDbContext dbContext;
+
+        public StudentServise(AppDbContext dbContext)
+            => this.dbContext = dbContext;
+        public async Task<Student> CreateStudent(CreateStudentDto newStudent)
         {
-            throw new NotImplementedException();
+            var student = new Student
+            {
+                Id = Guid.NewGuid(),
+                Fullname = newStudent.Fullname,
+                PhoneNumber = newStudent.PhoneNumber,
+                Age = newStudent.Age,
+                Direction = newStudent.Direction,
+                Degree = newStudent.Degree,
+                FacultyId = newStudent.FacultyId
+            };
+
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
+            return student;
         }
 
-        public Task<bool> DeleteStudent(Guid id)
+        public async Task<bool> DeleteStudent(Guid id)
         {
-            throw new NotImplementedException();
+            var student = await dbContext.Students
+                .Where(s => s.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (student is null)
+                return false;
+
+            dbContext.Students.Remove(student);
+            await dbContext.SaveChangesAsync();
+
+            return true;
         }
 
-        public Task<List<Student>> GetAllStudents()
+        public async Task<List<Student>> GetAllStudents()
         {
-            throw new NotImplementedException();
+            var student = await dbContext.Students
+                .Include(s => s.Faculty)
+                .ToListAsync();
+
+            if (student is null)
+                return null;
+
+            return student;
         }
 
-        public Task<GetStudentDto> GetStudent(Guid id)
+        public async Task<GetStudentDto> GetStudent(Guid id)
         {
-            throw new NotImplementedException();
+            var student = await dbContext.Students
+                .Where(s => s.Id == id)
+                .Include(s => s.Faculty)
+                .Include(s => s.Courses)
+                .FirstOrDefaultAsync();
+
+            if (student is null)
+                return null;
+
+            var result = new GetStudentDto(student);
+            return result;
         }
 
-        public Task<Student> UpdateStudent(Guid id, UpdateStudentDto student)
+        public async Task<Student> UpdateStudent(Guid id, UpdateStudentDto student)
         {
-            throw new NotImplementedException();
+            var updated = await dbContext.Students
+                .Where(s => s.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (updated is null)
+                return null;
+
+            updated.Fullname = student.Fullname;
+            updated.PhoneNumber = student.PhoneNumber;
+            updated.Age = student.Age;
+            updated.Direction = student.Direction;
+            updated.Degree = student.Degree;
+            updated.FacultyId = student.FacultyId;
+
+            await dbContext.SaveChangesAsync();
+            return updated;
         }
     }
 }
